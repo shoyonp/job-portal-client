bd30aef3e00552067170b6473410314449b5f04a
shoyonp <shoyonkumar143@gmail.com>
2024-12-12 03:20:47 +0600

    custom hook done

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
   const { id } = useParams();
-  console.log(id);
+  const { user } = useAuth();
+//   console.log(id,user);
 
   const submitJobApplication = (e) => {
     e.preventDefault();
@@ -12,64 +14,63 @@ const JobApply = () => {
     const github = form.github.value;
     const resume = form.resume.value;
 
-    console.log(linkedin, github, resume);
+    // console.log(linkedin, github, resume);
+
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedin,
+      github,
+      resume,
+    };
   };
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+    <div className="card bg-base-100 w-1/2 shadow-2xl mx-auto">
+      <h1 className="text-5xl font-bold text-center">
+        Apply Job and good luck!
+      </h1>
+      <form onSubmit={submitJobApplication} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Linkedin URL</span>
+          </label>
+          <input
+            type="url"
+            placeholder="Linkedin URL"
+            name="linkedin"
+            className="input input-bordered"
+            required
+          />
         </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form onSubmit={submitJobApplication} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Linkedin URL</span>
-              </label>
-              <input
-                type="url"
-                placeholder="Linkedin URL"
-                name="linkedin"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Github URL</span>
-              </label>
-              <input
-                type="url"
-                placeholder="Github URL"
-                name="github"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Resume URL</span>
-              </label>
-              <input
-                type="url"
-                placeholder="Resume URL"
-                name="resume"
-                className="input input-bordered"
-                required
-              />
-            </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Github URL</span>
+          </label>
+          <input
+            type="url"
+            placeholder="Github URL"
+            name="github"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Resume URL</span>
+          </label>
+          <input
+            type="url"
+            placeholder="Resume URL"
+            name="resume"
+            className="input input-bordered"
+            required
+          />
+        </div>
 
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Apply</button>
-            </div>
-          </form>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Apply</button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
