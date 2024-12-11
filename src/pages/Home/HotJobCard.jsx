44886c1e1aec4c01e9ac2594646a5a2feafddc77
shoyonp <shoyonkumar143@gmail.com>
2024-12-11 23:31:49 +0600

    jobs card doe

--- src/pages/Home/HotJobCard.jsx
+++ src/pages/Home/HotJobCard.jsx
@@ -1,28 +1,47 @@
 import React from "react";
+import { FaDollarSign, FaMapMarkerAlt } from "react-icons/fa";
 
 const HotJobCard = ({ job }) => {
   const {
     title,
     company,
     company_logo,
-    requirments,
+    requirements,
     description,
     location,
     salaryRange,
   } = job;
   return (
-    <div className="card card-compact bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
-      </figure>
+    <div className="card card-compact bg-base-100 shadow-xl">
+      <div className="flex gap-2 m-2 items-center">
+        <figure>
+          <img className="w-16" src={company_logo} alt="Shoes" />
+        </figure>
+        <div>
+          <h4 className="text-2xl">{company}</h4>
+          <p className="flex gap-1 items-center">
+            {" "}
+            <FaMapMarkerAlt></FaMapMarkerAlt> {location}
+          </p>
+        </div>
+      </div>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <h2 className="card-title">
+          {title}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+
+        <p>{description}</p>
+        <div className="flex gap-2 flex-wrap ">
+          {requirements?.map((skill) => (
+            <p className="border rounded-md text-center px-2 hover:text-amber-400-400 hover:bg-gray-400">
+              {skill}
+            </p>
+          ))}
+        </div>
+        <div className="card-actions justify-end items-center mt-4">
+            <p className="flex items-center">Salary: <FaDollarSign></FaDollarSign> {salaryRange.min} - {salaryRange.max} {salaryRange.currency}</p>
+          <button className="btn btn-primary">Apply</button>
         </div>
       </div>
     </div>
