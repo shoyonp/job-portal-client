8d25daa80a31968ba6180ef1bf641b48a149454f
shoyonp <shoyonkumar143@gmail.com>
2024-12-13 02:05:36 +0600

    application count done

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -13,6 +13,29 @@ const MyPostedJobs = () => {
   return (
     <div>
       <h2>My Posted jobs: {jobs.length} </h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Job Title</th>
+              <th>Deadline</th>
+              <th>Application count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs?.map((job, index) => (
+              <tr key={job._id} className="bg-base-200">
+                <th>{index + 1}</th>
+                <td>{job.title}</td>
+                <td>{job.applicationDeadline}</td>
+                <td>{job.applicationCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
